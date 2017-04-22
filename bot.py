c73cb584e196695fcab371957d872b7e4401be79
porosya80 <porosya@gmail.com>
2017-04-22 08:17:52 +0300

    add .gitignore

--- bot.py
+++ bot.py
@@ -4,4 +4,8 @@ import ENV
 token = ENV.token
 beerbot = telepot.Bot(token)
 # print (beerbot.getMe())
-beerbot.getUpdates()
+# print(beerbot.getUpdates())
+# beerbot.sendMessage(137199697, "Hello")
+
+# beerbot.setWebhook("https://5sfvlwn0sb.execute-api.us-west-2.amazonaws.com/v0/beerbot")
+print(beerbot.getWebhookInfo())
